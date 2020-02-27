af985a3a670de01083f165407a0928e7090d733a
eyvonne <eyvonne@users.noreply.github.com>
2020-02-26 20:17:09 -0800

    More testing!

--- Test/test_api.py
+++ Test/test_api.py
@@ -1,6 +1,3 @@
-import os
-import tempfile
-
 import pytest
 
 from QuakeAPI import APP
@@ -17,4 +14,19 @@ def test_base_route(client):
     response = client.get('/')
     print(response.data)
     message = b'{"message":"success, the flask API is running","status_code":200}\n'
+    assert message in response.data
+
+
+def test_test_route(client):
+    ''' this is actually testing the query_one function it occurs to me '''
+    response = client.get('/test')
+    print(response.data)
+    message = b'[3,"5km WNW of Cobb, CA",1582252014390,-122.776001,38.8348351,0.4,false]\n'
+    assert message in response.data
+
+
+def test_bad_time(client):
+    response = client.get('/last/fail')
+    message = b'{"message":"please choose from \\"hour\\", \\"day\\",\\n                                        \\"week\\", or \\"month\\"","status_code":400}\n'
+    print(response.data)
     assert message in response.data
