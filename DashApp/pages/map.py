ab695bdf4621669e52e98607e7c9bbe032bd391d
eyvonne <eyvonne@users.noreply.github.com>
2020-03-04 19:59:17 -0800

    breaking the map

--- DashApp/pages/map.py
+++ DashApp/pages/map.py
@@ -21,49 +21,70 @@ column1 = dbc.Col(
             to see earthquakes for.
             """
         ),
+        html.Div([
+            dcc.Dropdown(
+                id='timeFrame',
+                options=[
+                    {'label': 'Last Quake', 'value': 'lastQuake'},
+                    {'label': 'Last Hour', 'value': 'last/hour'},
+                    {'label': 'Last Day', 'value': 'last/day'},
+                    {'label': 'Last Week', 'value': 'last/week'},
+                    {'label': 'Last Month', 'value': 'last/month'}
+                ],
+                value='lastQuake'
+            ),
+            html.Div(id='menuItems')
+        ])
     ],
     md=2,
 )
 
+#fig = go.Figure()
 
-data = requests.get('https://quake-ds-production.herokuapp.com/last/day')
-df = pd.DataFrame(data.json()['message'])
 
-df['lat'] = df['latitude'].apply(lambda x: str(x))
-df['lon'] = df['longitude'].apply(lambda x: str(x))
+@app.callback(
+    dash.dependencies.Output('menuItems', 'fig'),
+    [dash.dependencies.Input('timeFrame', 'value')])
+def update_output(value):
+    data = requests.get(f'https://quake-ds-production.herokuapp.com/{value}')
+    print(data.json())
+    df = pd.DataFrame(data.json()['message'])
 
-data = [
-    go.Scattermapbox(
-        lat=df['lat'],
-        lon=df['lon'],
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=14
+    df['lat'] = df['latitude'].apply(lambda x: str(x))
+    df['lon'] = df['longitude'].apply(lambda x: str(x))
+
+    data = [
+        go.Scattermapbox(
+            lat=df['lat'],
+            lon=df['lon'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=14
+            ),
+            text=df['place'],
+        )
+    ]
+
+    layout = go.Layout(
+        autosize=True,
+        hovermode='closest',
+        mapbox=go.layout.Mapbox(
+            bearing=0,
+            center=go.layout.mapbox.Center(
+                lat=0,
+                lon=0
+            ),
+            pitch=0,
+            zoom=1
         ),
-        text=df['place'],
     )
-]
+    fig = go.Figure(data=data, layout=layout)
+    fig.update_layout(mapbox_style='stamen-terrain')
+    return data, layout
 
-layout = go.Layout(
-    autosize=True,
-    hovermode='closest',
-    mapbox=go.layout.Mapbox(
-        bearing=0,
-        center=go.layout.mapbox.Center(
-            lat=0,
-            lon=0
-        ),
-        pitch=0,
-        zoom=1
-    ),
-)
-
-fig = go.Figure(data=data, layout=layout)
-
-fig.update_layout(mapbox_style='stamen-terrain')
 
 column2 = dbc.Col([
-    dcc.Graph(figure=fig),
+    dcc.Graph(id='fig'),
 ])
 
 layout = dbc.Row([column1, column2])
