c4818b35db55a1332f4977f8936658f8f0ceb47f
eyvonne <eyvonne@users.noreply.github.com>
2020-03-04 19:05:43 -0800

    Update USGSetl_test.py

--- Test/USGSetl_test.py
+++ Test/USGSetl_test.py
@@ -13,6 +13,17 @@ class USGSetl_Test(unittest.TestCase):
         self.assertEqual(set(check[0][1:]), set(fake_quakes[0].values()))
         query_all("delete from USGS where time=10")
 
+    def test_get_time(self):
+        times = get_last_times(1582761731160, 'HOUR')
+        static_time = [1582758264000, 1582758263830, 1582759368500,
+                       1582759486630, 1582759368510, 1582759918440,
+                       1582760961770, 1582760961780, 1582761179520,
+                       1582761165689, 1582761179530, 1582761165768,
+                       1582761712010, 1582761731160, 1582759889310,
+                       1582761026296, 1582762288750]
+        for time, static in zip(times, static_time):
+            self.assertEqual(time, static)
+
 
 if __name__ == "__main__":
     unittest.main()
