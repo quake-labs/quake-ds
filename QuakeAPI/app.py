e951f100273ca89e3e902b4c9c9dc743070a1ad3
ash12hub <ash12hub@users.noreply.github.com>
2020-03-11 15:35:53 +0530

    Updated history function in app.py.

--- QuakeAPI/app.py
+++ QuakeAPI/app.py
@@ -1,3 +1,4 @@
+from math import degrees, radians, cos, sin, asin, atan2, sqrt
 from flask import Flask, jsonify
 from .USGSetl import *
 
@@ -51,9 +52,34 @@ def create_app():
         response = query_one('SELECT * FROM USGS where time=1582252014390')
         return jsonify(response)
 
-    @app.route('/history/<lat>,<lon>,<dist>')
+    @app.route('/history/<float:lat>,<float:lon>,<float:dist>')
     def history(lat, lon, dist):
-        # oof I just googled it and this is gonna be rough
-        pass
+        '''Calculate the top left and bottom right coordinates at distance dist
+        from coordinates lat, lon'''
+
+        lat = radians(lat)
+        lon = radians(lon)
+        radius = 6371
+        top_left = 315
+        bottom_right = 135
+
+        lat2A = asin(sin(lat) * cos(dist/radius) +
+                     cos(lat) * sin(dist/radius) * cos(top_left))
+        lat2B = asin(sin(lat) * cos(dist/radius) +
+                     cos(lat) * sin(dist/radius) * cos(bottom_right))
+
+        lon2A = lon + atan2(sin(top_left) * sin(dist/radius) *
+                            cos(lat), cos(dist/radius) - sin(lat) * sin(lat2A))
+        lon2B = lon + atan2(sin(bottom_right) * sin(dist/radius) *
+                            cos(lat), cos(dist/radius) - sin(lat) * sin(lat2B))
+
+        coordinates = {
+            "lonA": degrees(lon2A),
+            "latA": degrees(lat2A),
+            "lonB": degrees(lon2B),
+            "latB": degrees(lat2B)
+        }
+
+        return jsonify(coordinates)
 
     return app
